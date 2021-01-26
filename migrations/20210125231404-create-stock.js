48b4ee86e6f147c6149828a109abd4a139ce5d9d
ihjacobs925 <76010554+ihjacobs925@users.noreply.github.com>
2021-01-25 18:09:35 -0600

    stock model

---
+++ migrations/20210125231404-create-stock.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('stocks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      symbol: {
+        type: Sequelize.STRING
+      },
+      currentValue: {
+        type: Sequelize.INTEGER
+      },
+      closingValue: {
+        type: Sequelize.INTEGER
+      },
+      threeMonthValue: {
+        type: Sequelize.INTEGER
+      },
+      sixMonthValue: {
+        type: Sequelize.INTEGER
+      },
+      oneYearValue: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('stocks');
+  }
+};
